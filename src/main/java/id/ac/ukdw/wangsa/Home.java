6d8f388b9f76f91e4096ffe5321545d2adcffa73
damianwap <54344687+damianwap@users.noreply.github.com>
2019-11-18 19:33:44 +0700

    tambah controller home dan profil

---
+++ src/main/java/id/ac/ukdw/wangsa/Home.java
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.ac.ukdw.wangsa;
+
+import id.ac.ukdw.Koneksi.Konek;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author WINDOWS
+ */
+public class Home {
+
+    @FXML
+    private Label namalbl, profillbl, tambahlbl, katlbl, pengaturanlbl, tentanglbl, keluarlbl;
+    
+    @FXML
+    private TextField carifield;
+    
+    @FXML
+    private Button caribtn;
+    
+    Connection conn;
+    Statement st;
+    ResultSet rs;
+    
+        
+    public void setnama(String nama) {
+        this.namalbl.setText(nama);
+    }
+    
+    public void profil(){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/profil.fxml"));
+            Parent signin = (Parent) loader.load();
+            Profil hm = loader.getController();
+
+            conn = Konek.getConnect();
+            st = conn.createStatement();
+            rs = st.executeQuery("Select * from user where nama_user='" + this.namalbl.getText() + "'");
+            System.out.println("sampe sini");
+
+           // hm.setnamalengkap(this.namalbl.getText());
+            hm.setemail(rs.getString("email"));
+            hm.setjeniskel(rs.getString("Jenis_kelamin"));
+            hm.setnamalengkap(this.namalbl.getText());
+            hm.settanggal(rs.getString("tgl_lahir"));
+            Scene masuk = new Scene(signin);
+            Stage app_stage = (Stage) this.profillbl.getScene().getWindow();
+            app_stage.close();
+            app_stage.setScene(masuk);
+            app_stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                rs.close();
+                st.close();
+                conn.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    public void kategori(){
+         try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/kategori.fxml"));
+            Parent signin = (Parent) loader.load();
+            Kategori hm = loader.getController();
+          //  hm.setNama(this.namalbl.getText());
+            Scene masuk = new Scene(signin);
+            Stage app_stage = (Stage) this.katlbl.getScene().getWindow();
+            app_stage.close();
+            app_stage.setScene(masuk);
+            app_stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void tambahdata(){
+        
+    }
+    
+    public void tentang(){
+        
+    }
+    
+    public void keluar(){
+        
+    }
+    
+    public void cari(){
+        
+}
+    
+}
---
+++ src/main/java/id/ac/ukdw/wangsa/Profil.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.ac.ukdw.wangsa;
+
+import id.ac.ukdw.Koneksi.Konek;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author WINDOWS
+ */
+public class Profil {
+    @FXML 
+    private Label  namalengkaplbl, emaillbl, tgllbl, profillbl, pemasukanlbl, pengeluaranlbl, pengaturanlbl, tentanglbl, keluarlbl, jkllbl;
+    
+    @FXML
+    private Button hapusbtn, edit_user;
+    
+    Connection conn;
+    Statement st;
+    ResultSet rs;
+    
+       public void hapusbutton(ActionEvent ae){
+         conn = Konek.getConnect();
+         System.out.println("sampe hapus");
+        
+        
+        try{
+            st = conn.createStatement();
+            st.executeUpdate("delete from user where nama_user='"+this.namalengkaplbl.getText()+"'");
+            JOptionPane.showMessageDialog(null, "berhasil hapus");
+            FXMLLoader loader=new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
+                 Parent signin = (Parent) loader.load();
+                 Scene masuk = new Scene(signin);
+                 Stage app_stage  = (Stage) ((Node) ae.getSource()).getScene().getWindow();
+                 app_stage.close();
+                 app_stage.setScene(masuk);
+                 app_stage.show();
+        }catch(Exception e){
+            
+        }finally{
+            try{
+                conn.close();
+                rs.close();
+                st.close();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+       
+    public void edituser(){
+        
+    }
+       
+    public void setnamalengkap(String nama){
+        this.namalengkaplbl.setText(nama);
+    }
+
+    public void setemail(String nama){
+        this.emaillbl.setText(nama);
+    }
+    
+    public void settanggal(String nama){
+        this.tgllbl.setText(nama);
+    }
+
+    public void setjeniskel(String nama){
+        this.jkllbl.setText(nama);
+    }
+  
+}
+
